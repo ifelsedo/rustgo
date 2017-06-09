162b6c351947a40ccb3f8852709fb186568e4168
mikenut <mikerust@qq.com>
2017-06-09 22:56:09 +0800

    函数学习

--- proj/src/main.rs
+++ proj/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, Cargo! My name is Mike.");
+    let a: char = '💕';
+    println!("char is {}", a);
+    let i: i32 = 3;
+    let j: i32 = 9;
+    println!("{} + {} = {}", i, j, num_sum(i, j));
+}
+
+fn num_sum(i: i32, j: i32) -> i32 {
+    i + j
 }
